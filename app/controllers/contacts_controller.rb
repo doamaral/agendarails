bcb7577195d88300818bd05f0037dee81f546a6c
doamaral <lopesdoamaral@gmail.com>
2017-12-28 22:46:18 -0300

    i18n and complex forms using nested attributes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -15,6 +15,7 @@ class ContactsController < ApplicationController
   # GET /contacts/new
   def new
     @contact = Contact.new
+    @contact.build_address
     load_options_for_select
   end
 
@@ -71,7 +72,7 @@ class ContactsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
-      params.require(:contact).permit(:name, :email, :kind_id, :rmk)
+      params.require(:contact).permit(:name, :email, :kind_id, :rmk, address_attributes: [:street, :city, :state])
     end
 
     def load_options_for_select
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ApplicationRecord
   belongs_to :kind
+  has_one :address
+
+  accepts_nested_attributes_for :address
 end
